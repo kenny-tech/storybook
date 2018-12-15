927ecd037169d6b513007d225965948c993d1f6c
kenny-tech <kenny4real2001@gmail.com>
2018-12-15 13:20:10 +0100

    Handled auth

--- routes/auth.js
+++ routes/auth.js
@@ -5,4 +5,10 @@ const passport = require('passport');
 router.get('/google', passport.authenticate('google', 
 {scope: ['profile', 'email']}));
 
+router.get('/google/callback',
+    passport.authenticate('google', { failureRedirect : '/home' }),
+    (req, res) => {
+        res.redirect('/dashboard');
+    });
+
 module.exports = router;
