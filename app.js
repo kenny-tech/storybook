40b70f504ea472c2f7c675fde6cd93ea186a0edc
kenny-tech <kenny4real2001@gmail.com>
2018-12-15 11:55:21 +0100

    Create auth route

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+// Load Routes
+const auth = require('./routes/auth');
+
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('It works!')
 });
 
-const port = process.env.PORT || 5002;
+// Use Routes
+app.use('/auth', auth);
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://kenny:kenny4real@ds141950.mlab.com:41950/storybooks-dev',
+    googleClientID: '996966487798-r2h4u9sorrom21poef26bmshiub0fa0a.apps.googleusercontent.com',
+    googleClientSecret: '67oMixwbDKVM0jHm1QEceN9c'
+}
