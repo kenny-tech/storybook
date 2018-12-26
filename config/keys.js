63741f8456c4f629cc698ef659af99ad9bc277df
kenny-tech <kenny4real2001@gmail.com>
2018-12-26 23:47:14 +0100

    Updated website config

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb://kenny:kenny4real@ds141950.mlab.com:41950/storybooks-dev',
-    googleClientID: '996966487798-r2h4u9sorrom21poef26bmshiub0fa0a.apps.googleusercontent.com',
-    googleClientSecret: '67oMixwbDKVM0jHm1QEceN9c'
+if(process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev');
 }
